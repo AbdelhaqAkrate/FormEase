6920a05cb2643e4ad36bc28b15923bd20c2601c6
abdelhaq <abdelhaq.akrate20@gmail.com>
2023-02-17 16:27:31 +0100

    init project

---
+++ formease/src/main/java/com/ken/formease/FormeaseApplication.java
@@ -0,0 +1,13 @@
+package com.ken.formease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormeaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormeaseApplication.class, args);
+	}
+
+}
---
+++ formease/src/test/java/com/ken/formease/FormeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ken.formease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormeaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
